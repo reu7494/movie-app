4a6a1f509a3a1c3c70f492e9e0425169682061e5
black-ne-ko <reu7494@naver.com>
2025-07-08 14:11:36 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (query) => {
+    const movieID = query.movie.imdbID;
+    const res = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&i=${movieID}`
+    );
+    const data = await res.json();
+    setMovies(data.Search || []);
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>🎬 영화 검색</h2>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="영화 제목을 입력하세요"
+      />
+      <button
+        onClick={(e) => {
+          searchMovies(query);
+        }}
+      >
+        검색
+      </button>
+
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.imdbID}>
+            <strong>{movie.Title}</strong> ({movie.Year},{movie.imdbRating})
+            <br />
+            <img src={movie.Poster} alt={movie.Title} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
