6bc4e1bf62339bae7e5d5aae51a39cfa7c770d6e
black-ne-ko <reu7494@naver.com>
2025-07-09 16:31:26 +0900

    .

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ export default function App() {
 
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
+  const [moviesData, setMoviesData] = useState([]);
 
-  const searchMovies = async (query) => {
-    const movieID = query.movie.imdbID;
+  const searchMovies = async () => {
     const res = await fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&i=${movieID}`
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
     );
     const data = await res.json();
-    setMovies(data.Search || []);
-    console.log(data);
+    setMovies(data.Search);
+  };
+
+  const movieInformation = async (imdbID) => {
+    const res = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&i=${imdbID}`
+    );
+    const data = await res.json();
+    setMoviesData(data.Search);
   };
 
   return (
@@ -25,19 +32,12 @@ export default function App() {
         onChange={(e) => setQuery(e.target.value)}
         placeholder="영화 제목을 입력하세요"
       />
-      <button
-        onClick={(e) => {
-          searchMovies(query);
-        }}
-      >
-        검색
-      </button>
+      <button onClick={searchMovies}>검색</button>
 
       <ul>
         {movies.map((movie) => (
           <li key={movie.imdbID}>
-            <strong>{movie.Title}</strong> ({movie.Year},{movie.imdbRating})
-            <br />
+            <strong>{movie.Title}</strong> ({movie.Year})<br />
             <img src={movie.Poster} alt={movie.Title} />
           </li>
         ))}
